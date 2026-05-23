4e1fe7aeb0d769624d3e9d7dde3d004d41b9af5e
engnaser2005-lgtm <engnaser2005@gmail.com>
2026-05-23 14:13:34 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#041C3A',
+        header: '#02152d',
+        gold: '#D4AF37',
+        card: '#06264D',
+        secondary: '#0b2f5c',
+        'text-secondary': '#ddd',
+        danger: '#dc2626',
+        success: '#10b981',
+        warning: '#f59e0b',
+      },
+      fontFamily: {
+        tajawal: ['Tajawal', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [require('tailwindcss-rtl')],
+}
